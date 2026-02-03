79f37e409648387c562bc655c392a72271e06bbf
Mohamed Atef <www.xzorrodx@gmail.com>
2026-02-03 02:44:39 +0200

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/antigravity-nexus-mobile/sw.js', { scope: '/antigravity-nexus-mobile/' })})}
